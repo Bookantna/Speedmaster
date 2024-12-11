4ec8ea81a033773562b1980274a98585de0cce45
Thanawat Ueasiripracha <thanawatueasiripracha@Thanawats-MacBook-Air.local>
2024-12-11 12:32:39 +0700

    Use index as layout

--- speedmaster/speedmaster/views.py
+++ speedmaster/speedmaster/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     context = {'message': 'This is a dynamic message!'}
-    return render(request, 'index.html',{"Message":"Hello"})
+    return render(request, 'main.html',{"Message":"Hello"})
 
